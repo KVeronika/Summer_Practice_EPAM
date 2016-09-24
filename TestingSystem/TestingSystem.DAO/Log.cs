332292b90b02031334bba766efa64e238c520582
KVeronika <Veronika.cuzmina2011@yandex.ru>
2016-09-24 11:19:02 +0300

    add Log.cs

---
+++ TestingSystem/TestingSystem.DAO/Log.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace TestingSystem.DAO
+{
+    internal class Log
+    {
+        static Log()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static ILog For(object loggedObject)
+        {
+            if (loggedObject != null)
+            {
+                return For(loggedObject.GetType());
+            }
+            else
+            {
+                return For(null);
+            }
+        }
+
+        public static ILog For(Type objectType)
+        {
+            if (objectType != null)
+            {
+                return LogManager.GetLogger(objectType.Name);
+            }
+            else
+            {
+                return LogManager.GetLogger(string.Empty);
+            }
+        }
+    }
+}
